defc97ed0ad8d033a1f4c5bf0f73ed8f183f8839
fisnikHaziri <fisnik.haziri.h@gmail.com>
2025-01-10 18:30:19 +0100

    added the FocusedLesson component to display the selected lesson ddetails

--- src/components/FocusedLesson.tsx
+++ src/components/FocusedLesson.tsx
@@ -7,17 +7,32 @@ interface FocusedLessonProps {
 
 const FocusedLesson: React.FC<FocusedLessonProps> = ({ lesson }) => {
 	return (
-		<div id="FocusedLesson" className="w-screen flex flex-col p-5">
+		<div
+			id="FocusedLesson"
+			className="w-screen flex flex-col p-5 bg-stone-200 min-h-screen"
+		>
 			<h1 className="text-[40px] font-semibold pl-4">{lesson.title}</h1>
 			<hr id="TheIconicHr" />
-			<section>
-				<ul>
-					{lesson.keyPoints.map((point) => {
-						return <li>{point}</li>
-					})}
-				</ul>
-			</section>
-			<main>{lesson.content}</main>
+			{lesson.keyPoints.length > 0 && (
+				<section className="bg-stone-300 p-5 my-6 border rounded-[7px]">
+					<h3 className="mb-3 text-[25px] font-semibold">Key points</h3>
+					<ul className="pl-5 ml-10">
+						{lesson.keyPoints.map((point, index) => {
+							return (
+								<li
+									key={`${index}.${point[0]}`}
+									className="list-[square] mb-2 text-[18px] tracking-wide"
+								>
+									{point}
+								</li>
+							)
+						})}
+					</ul>
+				</section>
+			)}
+			<main className="bg-stone-300 p-5 my-6 border rounded-[7px]">
+				{lesson.content}
+			</main>
 		</div>
 	)
 }
